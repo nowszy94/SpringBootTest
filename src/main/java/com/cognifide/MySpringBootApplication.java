ed27fe3f13524446c3258894a97ea95e58b9d865
nowszy94 <nowszy94@gmail.com>
2016-04-19 16:18:59 +0200

    Changed main class name to MySpringBootApplication. Removed comment in dataInitializer method, and changed that method name to more unequivocal. In pom.xml, created property section including lombok, and spring-boot versions.

--- src/main/java/com/cognifide/MySpringBootApplication.java
+++ src/main/java/com/cognifide/MySpringBootApplication.java
@@ -5,26 +5,26 @@ import com.cognifide.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Created by cognifide.nowak on 01.04.2016.
  */
-@org.springframework.boot.autoconfigure.SpringBootApplication
-public class SpringBootApplication {
+@SpringBootApplication
+public class MySpringBootApplication {
 
     @Autowired
     private BookRepository bookRepository;
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(SpringBootApplication.class, args);
+        SpringApplication.run(MySpringBootApplication.class, args);
     }
 
     @Bean
-    public CommandLineRunner dataInitializer(BookRepository br) {
+    public CommandLineRunner bookRepositorydataInitializer(BookRepository br) {
         return new CommandLineRunner() {
             public void run (String... args) throws Exception {
-                // initialize bookRepository
                 bookRepository.save(Book.builder().title("W pustyni i w puszczy").author("Henryk Sienkiewicz").pages(400).build());
                 bookRepository.save(Book.builder().title("Dziady IV").author("Adam Mickiewicz").pages(300).build());
                 bookRepository.save(Book.builder().title("Krzyzacy").author("Henryk Sienkiewicz").pages(900).build());
